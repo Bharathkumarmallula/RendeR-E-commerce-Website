eddacb9786e80a5856b892c773f46877522a251b
Bharathkumarmallula <bharathkumar11998@gmail.com>
2025-03-04 16:20:14 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,8 +1,8 @@
-import {v2 as clodinary} from 'cloudinary';
+import {v2 as cloudinary} from 'cloudinary';
 
 const connectCloudinary = async() =>{
 
-    connectCloudinary.config({
+    cloudinary.config({
         cloud_name:process.env.CLOUDINARY_NAME,
         api_key:process.env.CLOUDINARY_API_KEY,
         secret_api_key: process.env.CLOUDINARY_SECRET_KEY
