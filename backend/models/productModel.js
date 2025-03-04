70d03c86720fbcd8c980e6eb9eaf7521b87c3fc3
Bharathkumarmallula <bharathkumar11998@gmail.com>
2025-03-04 13:08:20 +0530

    Adding the node & MongoDB

---
+++ backend/models/productModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{type:string, required:true},
+    description:{type:string, required:true},
+    price:{type:number,required:true},
+    image:{type:array,required:true},
+    category:{type:string,required:true},
+    subCategory:{type:string,required:true},
+    sizes:{type:array,required:true},
+    bestSeller:{type:boolean},
+    date:{type:number,required:true}
+
+})
+
+const productModel = mongoose.models.product || mongoose.model("product",productSchema)
+
+export default productModel;
